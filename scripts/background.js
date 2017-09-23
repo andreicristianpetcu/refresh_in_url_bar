427c1a97ac8c16847b5d7e51e446ac14128205ef
Andrei Cristian Petcu <andrei@ceata.org>
2017-09-23 14:04:18 +0300

    first commit

---
+++ scripts/background.js
@@ -0,0 +1,19 @@
+'use strict';
+
+browser.pageAction.onClicked.addListener(function(){
+  browser.tabs.query({active: true, currentWindow: true}).then((activeTab) => {
+    browser.tabs.reload(activeTab.id);
+  });
+});
+
+browser.tabs.onUpdated.addListener((id, changeInfo, tabInfo) => {
+    browser.pageAction.show(tabInfo.id);
+});
+
+browser.tabs.query({}).then((tabs) => {
+  var tab;
+  for (tab of tabs) {
+    browser.pageAction.show(tab.id);
+  }
+});
+
