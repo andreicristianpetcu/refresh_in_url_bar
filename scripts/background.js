8981a32b583cd9b520f72df30def11575a39e018
mpedrero <mpedrero10@gmail.com>
2017-11-16 00:13:18 +0100

    Switch icons on load and add stop button Update icons Update permission, mandatory to stop page loading Update COPYRIGHT to credit icons author Update version

--- scripts/background.js
+++ scripts/background.js
@@ -1,15 +1,29 @@
 'use strict';
 
-browser.pageAction.onClicked.addListener(function(){
-  browser.tabs.query({active: true, currentWindow: true}).then((activeTab) => {
-    browser.tabs.reload(activeTab.id);
-  });
-});
+browser.pageAction.onClicked.addListener(reloadTab);
 
+// Reload or stop the page depending on the status
+function reloadTab(tab) {
+  if (tab.status === 'complete') {
+    browser.tabs.reload(tab.id);
+  }
+  else {
+    browser.tabs.executeScript({code: "window.stop()", runAt: "document_start"});
+  }
+};
+
+// Update the icons depending if page is loading or loaded
 browser.tabs.onUpdated.addListener((id, changeInfo, tabInfo) => {
+    if (tabInfo.status === 'complete') {
+      browser.pageAction.setIcon({tabId: id, path: "images/refresh.svg"});
+    }
+    else {
+      browser.pageAction.setIcon({tabId: id, path: "images/cancel.svg"});
+    }
     browser.pageAction.show(tabInfo.id);
 });
 
+// Add icon in navigation bar
 browser.tabs.query({}).then((tabs) => {
   var tab;
   for (tab of tabs) {
